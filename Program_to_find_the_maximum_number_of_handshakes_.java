1e0ec41463645866638155d522c3170b439c9932
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-09-13 20:54:36 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,c;         a=sc.nextInt();         c=a*(a-1)/2;         System.out.println(c);
        }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,c;
+        a=sc.nextInt();
+        c=a*(a-1)/2;
+        System.out.println(c);
+        
+    }
+}
