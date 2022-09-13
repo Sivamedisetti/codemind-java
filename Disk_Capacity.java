11c1bba4cbd40a8e3b8c52d961e7bb770991cd92
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-09-13 21:08:54 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t,s,b,d;         t=sc.nextInt();         s=sc.nextInt();         b=sc.nextInt();         sc.close();         System.out.println(2*t*s*b*512);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t,s,b,d;
+        t=sc.nextInt();
+        s=sc.nextInt();
+        b=sc.nextInt();
+        sc.close();
+        System.out.println(2*t*s*b*512);
+        
+    }
+}
