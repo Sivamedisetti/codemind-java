84e81b2763acea4471fa1edada309cc825a19c20
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-09-20 21:22:29 +0530

    import java.util.*; class Descending {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int []arry=new int[n];         int a=1;         for(int i=0;i<n;i++)         {             arry[i]=sc.nextInt();         }         for(int i=0;i<n-1;i++)         {             if(arry[i]>arry[i+1])             {                 a=1;             }             else             {                 a=0;                 break;
                }
            }
            if(a==1)
            System.out.print("yes");
            if(a==0)
            System.out.print("no");
        }
    }

---
+++ Descending_array.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Descending
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arry=new int[n];
+        int a=1;
+        for(int i=0;i<n;i++)
+        {
+            arry[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(arry[i]>arry[i+1])
+            {
+                a=1;
+            }
+            else
+            {
+                a=0;
+                break;
+                
+            }
+        }
+        if(a==1)
+        System.out.print("yes");
+        if(a==0)
+        System.out.print("no");
+    }
+}
