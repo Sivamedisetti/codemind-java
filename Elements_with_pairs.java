7a63b6911dd081c2e2049ccc33dcc6234bd1da0c
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-09-22 21:48:09 +0530

    import java.util.*; class Element {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int []arr=new int [n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             System.out.print(arr[i]+" ");         }         if(n%2!=0)         {             System.out.print(0);         }     } }

---
+++ Elements_with_pairs.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Element
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int [n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        if(n%2!=0)
+        {
+            System.out.print(0);
+        }
+    }
+}
