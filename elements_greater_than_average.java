c9650a180adee5135c2afdd03c4f4889d79f18bc
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-09-26 21:54:19 +0530

    import java.util.*; class Solution {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int [n];         int s=0,k=0,t=0;         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             s+=arr[i];             k+=1;         }         for(int i=0;i<n;i++)         {            if(arr[i]>=(s/k))            {                t+=1;            }         }         System.out.println(t);
        }
    }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int [n];
+        int s=0,k=0,t=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            s+=arr[i];
+            k+=1;
+        }
+        for(int i=0;i<n;i++)
+        {
+           if(arr[i]>=(s/k))
+           {
+               t+=1;
+           }
+        }
+        System.out.println(t);
+        
+    }
+}
