8b0db85951ce1674d8d80a781d79bbc5c40676c3
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-10-14 20:07:46 +0530

    import java.util.*; class average {     public static void main(String asd[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i] = scan.nextInt();         }         scan.close();         int c=0;         for(int i=0;i<a;i++)         {             if(arr[i]%2==0)             {                 c=arr[i];             }         }         System.out.print(c);     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class average
+{
+    public static void main(String asd[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+        int c=0;
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                c=arr[i];
+            }
+        }
+        System.out.print(c);
+    }
+}
